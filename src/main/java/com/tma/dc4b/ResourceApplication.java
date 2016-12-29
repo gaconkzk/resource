cbaa768663295b9ebbaaaa87d500de95ad8149db
tntvu <tntvu@tma.com.vn>
2016-12-29 13:06:32 +0700

    working on session and resource

--- src/main/java/com/tma/dc4b/ResourceApplication.java
+++ src/main/java/com/tma/dc4b/ResourceApplication.java
@@ -5,18 +5,32 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ResourceApplication {
+public class ResourceApplication extends WebSecurityConfigurerAdapter {
 
-  @RequestMapping("/")
-  @CrossOrigin(origins = "*", maxAge = 3600)
+  @GetMapping("/")
+  @CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = {"x-auth-token", "x-requested-with"})
   public Message home() {
     return new Message("Hello World");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.cors().and().authorizeRequests().anyRequest().authenticated();
+  }
+
+  @Bean
+  HeaderHttpSessionStrategy sessionStrategy() {
+    return new HeaderHttpSessionStrategy();
   }
 
   public static void main(String[] args) {
